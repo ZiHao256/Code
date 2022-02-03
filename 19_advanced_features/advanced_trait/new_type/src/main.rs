017b92964b14a833b14bf4b3d2ee8a898e0b88fc
ZiHao256 <2638779206@qq.com>
2022-02-03 22:41:42 +0800

    更新19章

--- 19_advanced_features/advanced_trait/new_type/src/main.rs
+++ 19_advanced_features/advanced_trait/new_type/src/main.rs
@@ -1,8 +1,23 @@
+
 use std::fmt;
+use std::fmt::Formatter;
 
-struct Wrapper(Vec<String>);
+pub struct Wrapper(Vec<String>);
 
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        // write!(f, "({}, {}, {})", self.a, self.b, self.c);
+        for item in self.0.iter(){
+            write!(f,"{}, ",item);
+        }
+        Ok(())
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let mut w = Wrapper(vec!["1".to_string(),"2".to_string(),"3".to_string()]);
+
+    println!("{}",w);
+
+
 }
